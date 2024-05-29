4506ace6c30daaa2dca59b72fb1a9e4768469988
Aluno <>
2024-05-29 16:56:08 -0300

    Pronto para o video

--- TowerDefence/Assets/Scripts/Managers/GameManager.cs
+++ TowerDefence/Assets/Scripts/Managers/GameManager.cs
@@ -22,18 +22,20 @@ public class GameManager : MonoBehaviour
 
     public int playerSuprimentos = 3;
 
-    public int playermoral = 400;
+    public int playermoral = 700;
 
     public int waveatual = 1;
 
     public GameStateManager state;
 
-    
+    private UIManager ui;
 
     // Start is called before the first frame update
     void Start()
     {
-   //     ui = FindAnyObjectByType<UIManager>();
+        ui = FindAnyObjectByType<UIManager>();
+        ui.MudarMoral(playermoral);
+        ui.MudarSuplimentos(playerSuprimentos);
 
     }
 
@@ -46,7 +48,7 @@ public class GameManager : MonoBehaviour
     {
         gameData.playerSuprimentos -= suprimentos;
         playerSuprimentos = gameData.playerSuprimentos;
-     //   ui.MudarSuplimentos(playerSuprimentos);
+        ui.MudarSuplimentos(playerSuprimentos);
       //  if (gameData.playerSuprimentos <= 0)
        // {
             //GameOver
@@ -56,7 +58,7 @@ public class GameManager : MonoBehaviour
     public void AddMoral(int moral){
         gameData.playerMoral += moral;
         playermoral = gameData.playerMoral;
-       // ui.MudarMoral(playermoral);
+        ui.MudarMoral(playermoral);
     }
 
    public bool Checkprice(int value){
